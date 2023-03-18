e67d425912515cfbe819c026143c8bd7fb7d5b75
+ <+>
2023-03-18 15:39:38 -0400

    done

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+function reverse(word){
+  const createArray = word.split("")
+  const reversedArray = createArray.reverse()
+  const reversedWord = reversedArray.join("")
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  // create a function that returns true if a word is a palendrome or returns false if not
+  const reversedWord = reverse(word)
+  if (word === reversedWord){
+    return true;
+  } else {
+    return false ;
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string 
+  if the input string = reversed string then return true 
+  if they are not the same
+  returns false 
 */
 
 /*
   Add written explanation of your solution here
+  
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +41,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
